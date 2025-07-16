d729ac3b5c7d5b42ba450bf3a5554f712c25f247
HellblazerXXL <royceapera@yahoo.com>
2025-07-16 07:03:58 -0500

    Adds multiplication, division and exponentiation function

--- Day05/calc.py
+++ Day05/calc.py
@@ -15,5 +15,27 @@ print("Enter two numbers to subract")
 # Prompt user to enter two numbers
 first_number = float(input("Enter the first number : "))
 second_number = float(input("Enter the second number : "))
-summation = first_number - second_number
-print(f"{first_number} - {second_number} = {summation}")
+subtraction = first_number - second_number
+print(f"{first_number} - {second_number} = {subtraction:.2f}")
+
+print("Enter two numbers to multiply")
+# Prompt user to enter two numbers
+first_number = float(input("Enter the first number : "))
+second_number = float(input("Enter the second number : "))
+multiplication = first_number * second_number
+print(f"{first_number} * {second_number} = {multiplication:.2f}")
+
+print("Enter two numbers to divide")
+# Prompt user to enter two numbers
+first_number = float(input("Enter the first number : "))
+second_number = float(input("Enter the second number : "))
+division = first_number / second_number
+print(f"{first_number} / {second_number} = {division:.2f}")
+
+print("Enter two numbers to exponate")
+# Prompt user to enter two numbers
+first_number = float(input("Enter the first number : "))
+second_number = float(input("Enter the second number : "))
+exponentiation = first_number ** second_number
+print(f"{first_number} ** {second_number} = {exponentiation:.2f}")
+
