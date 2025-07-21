ad2f0cd9ed88e6fd1dcb8310ea93f3328301dcb2
HellblazerXXL <royceapera@yahoo.com>
2025-07-21 13:29:04 -0500

    Day06

---
+++ Day06/_01_Comparison_operators.py
@@ -0,0 +1,25 @@
+# Comparison Operators
+operand1 = 10
+operand2 = 2
+
+#Is operand1 greater-than operand 2?
+print(operand1 > operand2)
+
+#Is operand1 greater-less than operand 2?
+print(operand1 < operand2)
+
+user_age = int(input("Enter your age: "))
+print(f"Is {user_age} greater than or eqauls to 18?", user_age >= 18)
+
+score = float(input("Enter a score: " ))
+#print(f"Is user {score} greater or equal to 70", score >= 70)
+print("Is user {} greater or equal to 70?: {}".format(score, score >= 70))
+
+pin = 4190
+print("Welcome to our ATM")
+user_pin = int(input("Enter your pin: "))
+print("Is the pin correct?:", user_pin == pin)
+
+phone_password = 41111
+user_input = int(input("Enter pin :\n>>>"))
+print(f"Is {user_input} equal to {phone_pasword}?:", user_input == phone_password)
---
+++ Day06/_02_logical_operators.py
@@ -0,0 +1,10 @@
+# Logical operators
+# Logical and
+user_score = int(input("Enter your score: "))
+print("Should your grade be A?: ", user_score >= 70 and user_score <= 100)
+
+print( 5 > 2 and 10 == 10)
+print(4 != 4 or 7 < 2 and 1 > 0)
+print(not 3 == 3)
+print(not(10 > 3))
+print(8 > 3 and 8 != 4)
