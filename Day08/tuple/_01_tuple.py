8dab3ed8e25aef83701ec0f238beb1ac82ac0d4f
HellblazerXXL <royceapera@yahoo.com>
2025-07-29 17:17:01 -0500

    Adds tuple

--- Day08/tuple/_01_tuple.py
+++ Day08/tuple/_01_tuple.py
@@ -1,5 +1,6 @@
 #Tuple: Ordered inmutable sequence of elements.
 # Tuples are not mutable
 # Tuples are declared using  parenthesis. ()
-a = ;
+a = ("a",)
 print(type(a))
+#To have an element become a tuple, use a trailing comma.
---
+++ Day09/dictionary/_01_dictionary.py
@@ -0,0 +1,3 @@
+
+login = {"success":True, "message":"Welcome!", "email":"testmail.x.com", "username":"MP"}
+print(login["username"], login["email"], login["success"])
