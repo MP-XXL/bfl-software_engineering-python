201af338af612b2485f21c8066ff1a1ca0eb95bf
MP-XXL <royceapera@yahoo.com>
2025-08-18 03:53:51 -0500

    Makes changes

--- Day13/looping/recharge.py
+++ Day13/looping/recharge.py
@@ -1,85 +1,129 @@
-balance = 10000
-count = 0
-while count < 5:
-	ussd = input("enter ussd code:\n")
-	if ussd != "*556#":
-		print ("invalid command try again")
-	else:	
-		
-		print('''
-	     1. Buy airtime
-	     2. buy data
-	     3. borrow services
-	     4. check balance
-	     5. stop
-
-
-               ''')
-		command = int(input("enter command:\n"))
-		if command < 1 or command > 5:
-			print("invalid command")
-		if command == 5:
-			break		
-		if command == 1:
-				
-			airtime = int(input("enter amount of airtime to recharge:\n"))
-			num = int(input("enter phone number:\n"))
-			if airtime > balance:
-				print("amount greater than {balance}")
-			else:
-				print(f"you recharged {airtime} to {num}")
-				balance -= airtime
-				print(f"your new balance is {balance}")
-		if command == 2:
-			print(''' 1. 1gb for 300
-				  2. 2gb for 500
-				  3. 3gb for 1000 
-
-			''')
-			data = int(input("enter command:\n"))
-			if data < 1 or data > 3:
-				print("invalid command")
-				
-			if data == 1:
-				numb = int(input("enter phone number:\n"))
-				print(f"you just recieved 1gb data to {numb}")
-				balance -= 300
-				print(f"your new balance is {balance}")
-			if data == 2:
-				numb = int(input("enter phone number:\n"))
-				print(f"you just recieved 2gb data to {numb}")
-				balance -= 500
-				print(f"your new balance is {balance}")
-			if data == 3:
-				numb = int(input("enter phone number:\n"))
-				print(f"you just recieved 1gb data to {numb}")
-				balance -= 1000
-				print(f"your new balance is {balance}")
-	
-
-
-					                                                 
-		if command == 3:
-			print('''
-		       	1. borrow airtime
-		       	2. borrow data
-
-		       	''')
-			borrow = int(input("enter command:\n"))	
-			if borrow < 1 or borrow > 2:
-				print("invalid command")
-			if borrow == 1:
-				airtime_borrow = int(input("enter amount of card you want to borrow:\n"))
-				aritime_num = int(input("enter phone number:\n"))
-				print(f"you borrowed {airtime_borrow} of airtime")
-			
-				if borrow == 2:
-					data_borrow = int(input("enter amount of data you want to borrow:\n"))
-					data_num = int(input("enter phone number:\n"))
-					print(f"you borrowed {data_borrow} of data")
-		if command == 4:
-			print(f"your account balance is {balance}")
-			break  
-				
-
-		
+import time
+pin = "*556#"
+airtime_balance = 1000
+while True:
+    #print("Dial *556# for self service")
+    dial = input("Dial *556# for self service  or 5 to cancel: ")
+    if dial == pin:
+        #dial_input = int(input("Choose number to carry out action: \n1. Buy Airtime \n2. Buy Data \n3. Borrow Data \n4. Check Balance \n5. Quit \n >>>>: "))
+        action = True
+        while action:
+            dial_input = input("Choose number to carry out action: \n1. Buy Airtime \n2. Buy Data \n3. Borrow Data \n4. Check Balance \n5. Quit \n >>>>: ")
+            if dial_input == "1":
+                recharge_amount = int(input("Please input amount of airtime you wish to purchase : "))
+                airtime_balance += recharge_amount
+                print("Recharging your phone with {:,.2f} airtime".format(recharge_amount))
+                time.sleep(2)
+                print("Recharge Successful!")
+                print("Your new balance is ", airtime_balance)
+            elif dial_input == "2":
+                data = True
+                while data:
+                    data_option = int(input("Select option to carry out action:\n1. 1000 for 1GB\n2. 2000 for 3.5GB\n3. 3000 for 8GB\n>>>: "))
+                    if data_option == 1:
+                        data_price = 1000
+                        if data_price <= airtime_balance:
+                            airtime_balance -= data_price
+                            print(f"Your just bought 1GB data for {data_price:,.2f}, your balance is {airtime_balance:,.2f}")
+                        elif data_price  > airtime_balance:
+                            print("Insufficient funds")
+                    elif data_option == 2:
+                        data_price = 2000
+                        if data_price <= airtime_balance:
+                            airtime_balance -= data_price
+                            print(f"Your just bought 3.5G data for {data_price:,.2f}, your balance is {airtime_balance:,.2f}")
+                        elif data_price  > airtime_balance:
+                            print("Insufficient funds")
+                    elif data_option == 3:
+                        data_price = 3000
+                        if data_price <= airtime_balance:
+                            airtime_balance -= data_price
+                            print(f"Your just bought 8GB data for {data_price:,.2f}, your balance is {airtime_balance:,.2f}")
+                        elif data_price  > airtime_balance:
+                            print("Insufficient funds")
+                    else:
+                        print("Invalid input")
+                    data = False
+            elif dial_input == "3":
+                borrow = True
+                while borrow:
+                    borrow_option = int(input("Select option to carry out transaction:\n1. To borrow airtime\n2. To borrow data\n>>> : "))
+                    if borrow_option == 1:
+                        borrow_airtime = True
+                        while borrow_airtime:
+                            borrow_airtime_option = int(input("Select option to borrow airtime:\n1. To borrow 1000\n2. To borrow 2000\n3. To borrow 3000\n>>> : "))
+                            if borrow_airtime_option == 1:
+                                borrowed_balance = 1000
+                                new_airtime_balance = airtime_balance + borrowed_balance
+                                print(f"You just borrowed {borrowed_balance} airtime")
+                                print(f"You new balance is {new_airtime_balance:,.2f}")
+                                airtime_balance -= new_airtime_balance
+                                print(f"Your actual balance is {airtime_balance}")
+                                borrow_airtime = False
+                                borrow = False
+                            if borrow_airtime_option == 2:
+                                borrowed_balance = 2000
+                                new_airtime_balance = airtime_balance + borrowed_balance
+                                print(f"You just borrowed {borrowed_balance} airtime")
+                                print(f"You new balance is {new_airtime_balance:,.2f}")
+                                airtime_balance -= new_airtime_balance
+                                print(f"Your actual balance is {airtime_balance}")
+                                borrow_airtime = False
+                                borrow = False
+                            if borrow_airtime_option == 3:
+                                borrowed_balance = 3000
+                                new_airtime_balance = airtime_balance + borrowed_balance
+                                print(f"You just borrowed {borrowed_balance} airtime")
+                                print(f"You new balance is {new_airtime_balance:,.2f}")
+                                airtime_balance -= new_airtime_balance
+                                print(f"Your actual balance is {airtime_balance}")
+                                borrow_airtime = False
+                                borrow = False
+                            else:
+                                print("Invalid Input")
+                    if borrow_option == 2:
+                        borrow_data = True
+                        while borrow_data:
+                            borrow_data_option = int(input("Select option to borrow airtime:\n1. To borrow 1GB for 1000\n2. To borrow 3.5GB for 2000\n3. To borrow 8GB for 3000\n>>> : "))
+                            if borrow_data_option == 1:
+                                borrowed_balance = 1000
+                                new_airtime_balance = airtime_balance + borrowed_balance
+                                print(f"You just borrowed 1GB data for {borrowed_balance}")
+                                airtime_balance -= new_airtime_balance
+                                print(f"Your actual balance is {airtime_balance}")
+                                borrow_data = False
+                                borrow = False
+                            if borrow_data_option == 2:
+                                borrowed_balance = 2000
+                                new_airtime_balance = airtime_balance + borrowed_balance
+                                print(f"You just borrowed 3.5GB data for {borrowed_balance}")
+                                airtime_balance -= new_airtime_balance
+                                print(f"Your actual balance is {airtime_balance}")
+                                borrow_data = False
+                                borrow = False
+                            if borrow_data_option == 3:
+                                borrowed_balance =3000
+                                new_airtime_balance = airtime_balance + borrowed_balance
+                                print(f"You just borrowed 8GB data for {borrowed_balance}")
+                                airtime_balance -= new_airtime_balance
+                                print(f"Your actual balance is {airtime_balance}")
+                                borrow_data = False
+                                borrow = False
+                            else:
+                                print("Invalid input!")
+            elif dial_input == "4":
+                print(f"Your airtime balance is {airtime_balance}")
+            elif dial_input == "5":
+                break
+            else:
+                print("Input unrecognized!")
+        break
+    elif dial == "5":
+        print("Canceling USSD operation...")
+        time.sleep(1)
+        print("...")
+        time.sleep(1)
+        print("Operation cancelled")
+        break
+    else:
+        print("Please dial the correct code! : ")
