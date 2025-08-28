adcb4fe0492bbe566c88067e0fed4c08919f4741
MP-XXL <royceapera@yahoo.com>
2025-08-28 10:30:31 -0500

    Adds second part

--- Day14/functions/assignment_3.py
+++ Day14/functions/assignment_3.py
@@ -9,8 +9,10 @@ def start():
         option = int(input("""
                 WELCOME TO THE IVENTORY
 
-                1. Enter \"1\" to add product to iventory
-                2. Enter \"2\" to update stock in inventory
+                1. Enter \"1\" to add product to iventory.
+                2. Enter \"2\" to update stock in inventory.
+                3. Enter \"3\" to initiate sale for a product.
+                4. Enter \"4\" to view current inventory.
                 \n>>> : """))
         user_choice(option)
 
@@ -24,6 +26,12 @@ def user_choice(choice_option):
         product = input("Enter name of product to update: ").capitalize()
         new_quantity = int(input("Enter the quantity to add or reduce : "))
         update_stock(store, product, new_quantity)
+    elif choice_option == 3:
+        product = input("Enter product name you want to sell : ").capitalize()
+        quantity_to_sell = int(input("Enter the quantity to sell : "))
+        sell_product(store, product, quantity_to_sell)
+    elif choice_option == 4:
+        display_inventory(store)
 # Adding a new product to the to inventory
 def add_product(store, name, price, quantity):
     if name in store:
@@ -36,14 +44,49 @@ def update_stock(store, name, quantity):
     if name not in store:
         print("Prouct not found! Try the add product option.")
     else:
-        update_math = int(input("Enter \"1\" to add to stock or \"2\" to reduce from stock : "))
-        if update_math == 1:
-            store[name]["quantity"] += quantity
-            print(f"Item updated successfully! {quantity} {store[name]} have been added to stock!")
-        elif update_math == 2:
+        while True:
+            update_math = int(input("Enter \"1\" to add to stock or \"2\" to reduce from stock : "))
+            if update_math == 1:
+                store[name]["quantity"] += quantity
+                print(f"Item updated successfully! {quantity} {store[name]} have been added to stock!")
+                print("UPDATED INVENTORY")
+                for item in store:
+                    print(f"Name = {item}: Price:{store[item]['price']}, Quantity:{store[item]['quantity']}")
+                break
+            elif update_math == 2:
+                if quantity > store[name]["quantity"]:
+                    print("Inputed quantity to reduce greater than available quantity. Please view total iventory for accurate stock value.")
+                else:
+                    store[name]["quantity"] -= quantity
+                    print(f"Item update successfully! {quantity} {store[name]} have been removed from stock!")
+                    print("UPDATED INVENTORY")
+                    for item in store:
+                        print(f"Name = {item}: Price:{store[item]['price']}, Quantity:{store[item]['quantity']}")
+
+                    break
+            else:
+                print("Invalid command!")
+# Selling a product
+def sell_product(store, name, quantity):
+    if name not in store:
+        print("Item is out of stock!")
+    else:
+        if quantity > store[name]["quantity"]:
+            print("Insufficient product quantity. Desired quantity to sell is greater than available quantity. Please view total inventory for accurate stock value.")
+        else:
             store[name]["quantity"] -= quantity
-            print(f"Item update successfully! {quantity} {store[name]} have been removed from stock!")
-    print("UPDATED INVENTORY")
-    for item in store:
-        print(f"Name = {item}: Price:{store[item]['price']}, Quantity:{store[item]['quantity']}")
+            print("Sale successful!")
+            total_sale = quantity * store[name]["price"]
+            print(f"A quantity of {quantity} {name} has/have been sold for the price of {store[name]['price'] :,.2f} each. Total sale price = {total_sale :,.2f}")
+# Display inventory
+def display_inventory(store):
+    print("LIST OF CURRENT INVENTORY")
+    for products in store:
+        print(f"""
+                ___
+                |
+                |Name: {products},\t\t\t Price: {store[products]['price']},\t\t\t Quantity: {store[products]['quantity']}
+                |__
+                """)
+    print("TOTAL NUMBER OF PRODUCTS")
 start()
